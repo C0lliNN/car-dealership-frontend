927d01fa69987d167793f482bf40c837cbd514b3
C0lliNN <rapphaelmanhaes2017@hotmail.com>
2022-08-27 14:41:06 -0300

    Create Simple Component

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,6 @@
 <template>
   <v-app>
-    <v-app-bar
-      app
-      color="primary"
-      dark
-    >
+    <v-app-bar app color="primary" dark>
       <div class="d-flex align-center">
         <v-img
           alt="Vuetify Logo"
@@ -38,23 +34,23 @@
     </v-app-bar>
 
     <v-main>
-      <HelloWorld/>
+      <UserCrud />
     </v-main>
   </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld';
+import UserCrud from './components/UserCrud.vue';
 
 export default {
   name: 'App',
 
   components: {
-    HelloWorld,
+    UserCrud
   },
 
   data: () => ({
     //
-  }),
+  })
 };
 </script>
